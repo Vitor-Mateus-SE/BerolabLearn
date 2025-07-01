ba4b779b3be94ee617110dd2fe2739e53b545524
Vitor Mateus Andrade <vitor.mateus.pessoal@gmail.com>
2025-07-01 10:55:37 -0300

    just a little

---
+++ Codes/First_classes/OnlyBrothers/handlers/ban.js
@@ -0,0 +1,25 @@
+//boolean logic
+//is true or false
+// > is bigger
+// < is lower
+// == is equal
+// ! is diff
+/*var activeUsers = 200;
+var publication = {
+  user: {
+    active: true,
+  },
+  title: "test",
+  likes: 10,
+  ban: 100,
+};*/
+
+function ban(publication, publication) {
+  var banIsHigherThan40 = (40 / 100) * activeUsers;
+
+  if (publication.ban > banIsHigherThan40) {
+    publication.user.active = false;
+  }
+  console.log("user is active?");
+  console.log(publication.user.active);
+}
--- Codes/First_classes/OnlyBrothers/index.js
+++ Codes/First_classes/OnlyBrothers/index.js
@@ -1,2 +1,8 @@
 //test commit
 var test = "is working?";
+//input - output
+//code
+function printHelloWorld(message) {
+  console.log(message);
+}
+printHelloWorld("test today");
